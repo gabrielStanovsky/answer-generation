4a85652a991ab1418ba01c634bbc8c56d1cd6c0f
Anthony Chen <anthony.w.chen@outlook.com>
2019-11-12 13:24:26 -0800

    added bt for all generative tasks

--- backtranslation/backtranslate_answers.py
+++ backtranslation/backtranslate_answers.py
@@ -6,58 +6,57 @@ import shutil
 backtranslation_repo_abs_dirname = '/home/tony/QAMetric/Backtranslation/'
 backtranslation_script_abs_filename = join(backtranslation_repo_abs_dirname, 'paraphrase_all_languages.py')
 
-def backtranslate_cosmosqa():
-	DATA_DIR = '/home/tony/answer-generation/data/cosmosqa/'
-	TRAIN_ANSWERS = join(DATA_DIR, 'train.csv_answers')
-	DEV_ANSWERS = join(DATA_DIR, 'dev.csv_answers')
-
-	if isfile(join(DATA_DIR, 'train.csv_answers.backtranslations')) and \
-		isfile(join(DATA_DIR, 'dev.csv_answers.backtranslations')):
+def backtranslate_file(DATA_DIR, ANSWER_FILE, OUTPUT_DIR):
+	# Check if `ANSWER_FILE` has already been backtranslated
+	if isfile(join(OUTPUT_DIR, ANSWER_FILE.split('/')[-1]+'.backtranslations')):
 		return 
 
-	system('cd ' + backtranslation_repo_abs_dirname + '; python ' + backtranslation_script_abs_filename + ' ' + TRAIN_ANSWERS)
-	system('cd ' + backtranslation_repo_abs_dirname + '; python ' + backtranslation_script_abs_filename + ' ' + DEV_ANSWERS)
-
-	# Move backtranslation files into the backtranslation directory
-	shutil.move(join(DATA_DIR, 'train.csv_answers.backtranslations'), 'backtranslation/cosmosqa/')
-	shutil.move(join(DATA_DIR, 'dev.csv_answers.backtranslations'), 'backtranslation/cosmosqa/')
+	# Backtranslate and move backtranslation file into `OUTPUT_DIR`
+	system('cd ' + backtranslation_repo_abs_dirname + '; python ' + backtranslation_script_abs_filename + ' ' + ANSWER_FILE)
+	shutil.move(ANSWER_FILE+'.backtranslations', OUTPUT_DIR)
 
 	# Delete intermediate backtranslation files
 	for f in glob.glob(join(DATA_DIR, '*csv_answers.*')):
 		remove(f)
+
+	# Merge backtranslations back into the data file
+
+def backtranslate_cosmosqa():
+	DATA_DIR = '/home/tony/answer-generation/data/cosmosqa/'
+	DEV_ANSWERS = join(DATA_DIR, 'dev.csv_answers')
+	OUTPUT_DIR = 'backtranslation/cosmosqa/'
+
+	backtranslate_file(DATA_DIR, DEV_ANSWERS, OUTPUT_DIR)
 
 def backtranslate_socialiqa():
 	DATA_DIR = '/home/tony/answer-generation/data/socialiqa/'
-	TRAIN_ANSWERS = join(DATA_DIR, 'train.csv_answers')
 	DEV_ANSWERS = join(DATA_DIR, 'dev.csv_answers')
 	TEST_ANSWERS = join(DATA_DIR, 'test.csv_answers')
+	OUTPUT_DIR = 'backtranslation/socialiqa/'
 
-	if isfile(join(DATA_DIR, 'train.csv_answers.backtranslations')) and \
-		isfile(join(DATA_DIR, 'dev.csv_answers.backtranslations')) and \
-		isfile(join(DATA_DIR, 'test.csv_answers.backtranslations')):
-		return 
-		
-	system('cd ' + backtranslation_repo_abs_dirname + '; python ' + backtranslation_script_abs_filename + ' ' + TRAIN_ANSWERS)
-	system('cd ' + backtranslation_repo_abs_dirname + '; python ' + backtranslation_script_abs_filename + ' ' + DEV_ANSWERS)
-	system('cd ' + backtranslation_repo_abs_dirname + '; python ' + backtranslation_script_abs_filename + ' ' + TEST_ANSWERS)
-
-	# Move backtranslation files into the backtranslation directory
-	shutil.move(join(DATA_DIR, 'train.csv_answers.backtranslations'), 'backtranslation/socialiqa/')
-	shutil.move(join(DATA_DIR, 'dev.csv_answers.backtranslations'), 'backtranslation/socialiqa/')
-	shutil.move(join(DATA_DIR, 'test.csv_answers.backtranslations'), 'backtranslation/socialiqa/')
-
-	# Delete intermediate backtranslation files
-	for f in glob.glob(join(DATA_DIR, '*csv_answers.*')):
-		remove(f)
+	backtranslate_file(DATA_DIR, DEV_ANSWERS, OUTPUT_DIR)
+	backtranslate_file(DATA_DIR, TEST_ANSWERS, OUTPUT_DIR)
 
 def backtranslate_narrativeqa():
-	pass
+	DATA_DIR = '/home/tony/answer-generation/data/narrativeqa/'
+	DEV_ANSWERS = join(DATA_DIR, 'dev.csv_answers')
+	TEST_ANSWERS = join(DATA_DIR, 'test.csv_answers')
+	OUTPUT_DIR = 'backtranslation/narrativeqa/'
+
+	backtranslate_file(DATA_DIR, DEV_ANSWERS, OUTPUT_DIR)
+	backtranslate_file(DATA_DIR, TEST_ANSWERS, OUTPUT_DIR)
 
 def backtranslate_mcscript():
-	pass
+	DATA_DIR = '/home/tony/answer-generation/data/mcscript/'
+	DEV_ANSWERS = join(DATA_DIR, 'dev.csv_answers')
+	TEST_ANSWERS = join(DATA_DIR, 'test.csv_answers')
+	OUTPUT_DIR = 'backtranslation/mcscript/'
+
+	backtranslate_file(DATA_DIR, DEV_ANSWERS, OUTPUT_DIR)
+	backtranslate_file(DATA_DIR, TEST_ANSWERS, OUTPUT_DIR)
 
 if __name__ == '__main__':
 	backtranslate_cosmosqa()
-	# backtranslate_socialiqa()
-	# backtranslate_narrativeqa()
-	# backtranslate_mcscript()
+	backtranslate_socialiqa()
+	backtranslate_narrativeqa()
+	backtranslate_mcscript()
